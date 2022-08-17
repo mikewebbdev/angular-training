680c0d56d805226f2a0e99ea3706ca13bde17f94
mikewebbdev <mike.webb@orlo.tech>
2022-08-17 17:15:43 +0100

    add 404 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule, Routes} from "@angular/router";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from "@angular/router";
 
-import {AppComponent} from './app.component';
-import {ServerComponent} from "./server-management/server/server.component";
-import {FormsModule} from "@angular/forms";
-import {ServersEditComponent} from './server-management/servers-edit/servers-edit.component';
-import {WarningAlertComponent} from './warning-alert/warning-alert.component';
-import {SuccessAlertComponent} from './success-alert/success-alert.component';
-import {GameControlComponent} from './events-game/game-control/game-control.component';
-import {OddComponent} from './events-game/odd/odd.component';
-import {EvenComponent} from './events-game/even/even.component';
-import {BasicHighlightDirective} from "./highlight-directive/basic-highlight/basic-highlight.directive";
-import {BetterHighlightDirective} from './highlight-directive/better-highlight/better-highlight.directive';
-import {HeaderComponent} from './header/header.component';
-import {ServerManagementComponent} from './server-management/server-management.component';
-import {EventsGameComponent} from './events-game/events-game.component';
-import {HighlightDirectiveComponent} from './highlight-directive/highlight-directive.component';
-import {AccountManagementComponent} from './account-management/account-management.component';
-import {AccountComponent} from "./account-management/account/account.component";
-import {NewAccountComponent} from "./account-management/new-account/new-account.component";
-import {AccountsService} from "./account-management/accounts.service";
-import {LoggingService} from "./account-management/logging.service";
+import { AppComponent } from './app.component';
+import { ServerComponent } from "./server-management/server/server.component";
+import { FormsModule } from "@angular/forms";
+import { ServersEditComponent } from './server-management/servers-edit/servers-edit.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { GameControlComponent } from './events-game/game-control/game-control.component';
+import { OddComponent } from './events-game/odd/odd.component';
+import { EvenComponent } from './events-game/even/even.component';
+import { BasicHighlightDirective } from "./highlight-directive/basic-highlight/basic-highlight.directive";
+import { BetterHighlightDirective } from './highlight-directive/better-highlight/better-highlight.directive';
+import { HeaderComponent } from './header/header.component';
+import { ServerManagementComponent } from './server-management/server-management.component';
+import { EventsGameComponent } from './events-game/events-game.component';
+import { HighlightDirectiveComponent } from './highlight-directive/highlight-directive.component';
+import { AccountManagementComponent} from './account-management/account-management.component';
+import { AccountComponent } from "./account-management/account/account.component";
+import { NewAccountComponent } from "./account-management/new-account/new-account.component";
+import { AccountsService } from "./account-management/accounts.service";
+import { LoggingService } from "./account-management/logging.service";
 import { UserManagementComponent } from './user-management/user-management.component';
 import { ActiveUsersComponent } from './user-management/active-users/active-users.component';
 import { InactiveUsersComponent } from './user-management/inactive-users/inactive-users.component';
-import {CounterService} from "./user-management/counter.service";
+import { CounterService } from "./user-management/counter.service";
 import { ServerRoutingComponent } from './server-routing/server-routing.component';
 import { HomeComponent } from './server-routing/home/home.component';
 import { ServersComponent } from './server-routing/servers/servers.component';
@@ -33,6 +33,7 @@ import { UsersComponent } from './server-routing/users/users.component';
 import { EditServerComponent } from './server-routing/servers/servers/edit-server/edit-server.component';
 import { RoutingServerComponent } from './server-routing/servers/servers/routing-server/routing-server.component';
 import { UserComponent } from './server-routing/users/user/user.component';
+import { PageNotFoundComponent } from './server-routing/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -43,7 +44,8 @@ const appRoutes: Routes = [
       { path: ':id', component: RoutingServerComponent },
       { path: ':id/edit', component: EditServerComponent }
     ] },
-
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -74,7 +76,8 @@ const appRoutes: Routes = [
     UsersComponent,
     EditServerComponent,
     RoutingServerComponent,
-    UserComponent
+    UserComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
